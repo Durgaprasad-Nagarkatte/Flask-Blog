7de110b7f1b5dc4f04eddd745d339662477c110b
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2019-11-05 22:17:33 +0530

    Templates Commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from flask import render_template
 from app import app
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 def index():
-    return '<h1>Hello, World!</h1>'
+    title = "Durgaprasad"
+    user = {"username": "Durgaprasad"}
+    posts = [
+        {
+            "author": "Akshatha",
+            "body": "What a beautiful day. !!"
+        },
+        {
+            "author": "Mangala",
+            "body": "Having a great time in Mumbai. !!"
+        }]
+    print(posts)
+    return render_template("index.html", user=user, title=title, posts=posts)
