99f0ac096a3abe993faf06f557068537ab42e8d9
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2019-11-06 02:42:38 +0530

    Forms added

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, flash, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
@@ -17,3 +18,11 @@ def index():
         }]
     print(posts)
     return render_template("index.html", user=user, title=title, posts=posts)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested from user {}'.format(form.username.data))
+        return redirect(url_for('index'))
+    return render_template("login.html", form=form, title='Sign In')      
