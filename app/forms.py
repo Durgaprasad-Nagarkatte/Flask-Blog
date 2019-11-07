8161aa34f64033617c08a98fcc930be4cc322a04
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2019-11-07 21:08:00 +0530

    Database added

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
+#from flask_wtf.recaptcha import RecaptchaField
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember me")
     submit = SubmitField("Sign In")
+    #recaptcha = RecaptchaField()
--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-forget'
+    RECAPTCHA_PUBLIC_KEY = "6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///" + os.path.join(BASEDIR, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
