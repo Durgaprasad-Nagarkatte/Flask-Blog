b8850875ae7fa180e4fb35331c26ae423e35fbde
Durgaprasad-Nagarkatte <durgaprasad.nagarkatte@gmail.com>
2019-11-09 22:24:21 +0530

    User Registration added.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from app.models import User
 #from flask_wtf.recaptcha import RecaptchaField
 
 class LoginForm(FlaskForm):
@@ -9,3 +10,21 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField("Remember me")
     submit = SubmitField("Sign In")
     #recaptcha = RecaptchaField()
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Repeat Password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address')
+
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, redirect, flash, url_for, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, current_user, login_user, logout_user, login_required
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 @app.route('/', methods=['GET', 'POST'])
@@ -44,4 +44,26 @@ def login():
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
     logout_user()
-    return redirect('login')     
+    return redirect('login')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        #user = User.query.filter_by(username=form.username.data).first()
+        #mail = User.query.filter_by(email=form.email.data).first()
+        #if user or email:
+        #    flash('Username or email already exists.')
+        #    return redirect(url_for('login'))
+        #if user is None and email is None:
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now registered with our site.')
+        return redirect(url_for('login'))
+    return render_template("register.html", form=form)     
